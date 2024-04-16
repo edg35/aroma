d8adafcbad306db061509bd66f09a3f9eb745fe1
edg35 <ethandavid111@gmail.com>
2024-04-16 12:10:36 -0400

    login page

--- aroma_app/aroma_flutter/lib/features/user_auth/presentation/pages/login_page.dart
+++ aroma_app/aroma_flutter/lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:aroma_flutter/features/user_auth/presentation/widgets/form_container_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -9,13 +10,50 @@ class LoginPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Login Page'),
         ),
-        body: const Column(
-          children: [
-            Text(
-              "Login",
-              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-            )
-          ],
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const FormContainerWidget(
+                  hintText: 'Email',
+                  isPasswordField: false,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const FormContainerWidget(
+                  hintText: 'Password',
+                  isPasswordField: true,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    width: double.infinity,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blue,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    )),
+              ],
+            ),
+          ),
         ));
   }
 }
